3fe5ecc61778abb6de8e61142b777b1832e86e61
alva.damian <alva.damian@yahoo.com.ar>
2017-09-15 19:19:34 -0400

    implmentacion de metodos para pasar los test 1 y 2

--- lib/hw1.rb
+++ lib/hw1.rb
@@ -5,20 +5,31 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 # Part 1
 # http://ruby-doc.org/core-2.2.0/Regexp.html
 def validate_email?(email)
-  # YOUR CODE HERE
+  (VALID_EMAIL_REGEX =~ email) != nil
 end
 
 def is_palindrome?(phrase)
-  # YOUR CODE HERE
+  
+  if phrase == ""
+    return false
+  end
+  
+  phrase.downcase.gsub(" ", "").reverse == phrase.downcase.gsub(" ", "")
 end
 
 # Part 2
 def max_sum_of_2(arr1, arr2)
-  # YOUR CODE HERE
+  if arr1.reduce(:+) > arr2.reduce(:+)
+    return arr1.reduce(:+)
+  elsif arr1.reduce(:+) == arr2.reduce(:+)
+    return 0
+  end
+  
+  return arr2.reduce(:+)
 end
 
 def word_frequency(words)
-  # YOUR CODE HERE
+  words.max { |a, b| a <=> b }
 end
 
 # Part 3
